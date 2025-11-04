063081cd143a18fa5df11425b2a8476d68fa81dc
4ntonoff <saneokantonov101@gmail.com>
2025-11-04 10:47:47 +0200

    Fix repository name in next.config.ts for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'portfolio';
+const repoName = 'Portfolio';
 
 const nextConfig: NextConfig = {
   output: 'export',
